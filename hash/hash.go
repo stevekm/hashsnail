3d26c733037bb5aac3d3728087fd34972baadd32
Stephen Kelly <stevekm@users.noreply.github.com>
2022-12-19 20:21:08 -0500

    start multithread concurrent execution

--- hash/hash.go
+++ hash/hash.go
@@ -1,6 +1,7 @@
 package hash
 
 import (
+	"log"
 	"fmt"
 	// "errors" //errors.New("No value found for hash")
 	"crypto/md5"
@@ -8,7 +9,15 @@ import (
 	// "crypto/sha1"
 	// "crypto/sha256"
 	"encoding/hex"
+	"runtime"
+	"sync"
+	"context"
 )
+
+type HashResult struct {
+	Hash string
+	Result string
+}
 
 type HashFinder struct {
 	NumCombs   int
@@ -19,6 +28,7 @@ type HashFinder struct {
 }
 
 func (f *HashFinder) Find() (string, error) {
+	// find the string combination that matches the wanted hash
 	for i := 0; i < f.NumCombs; i++ {
 		comb := f.combinator.Next()
 		if len(comb) > f.MaxSize {
@@ -33,6 +43,104 @@ func (f *HashFinder) Find() (string, error) {
 			break
 		}
 	}
+	return "", fmt.Errorf("No value found for hash %v", f.Wanted)
+}
+
+
+
+func (f *HashFinder) FindParallel() (string, error) {
+	numWorkers := 2
+	runtime.GOMAXPROCS(numWorkers) // runtime.NumCPU()
+
+	work := make(chan string) // send comb's in here
+	results := make(chan HashResult) // send hash results back out here
+	ctx, cancel := context.WithCancel(context.Background()) // signal to stop sending work
+
+	// create worker goroutines
+	wg := sync.WaitGroup{}
+	for i := 0; i < numWorkers; i++ {
+		// add a worker
+		wg.Add(1)
+		// add a copy of the goroutine
+		go func() {
+			defer wg.Done()
+			// get the next combination from the work channel
+			for comb := range work {
+				// make the hash
+				hash := GetHash(comb)
+				if f.Print {
+					fmt.Printf("%v %v\n", comb, hash)
+				}
+				// check if its the correct hash
+				if hash == f.Wanted {
+					result := HashResult{
+						Hash: hash,
+						Result: comb,
+					}
+					fmt.Printf("worker found a result: %v %v\n", result.Result, result.Hash)
+					// send the correct result to the output channel
+					results <- result
+					// stop sending work
+					cancel()
+					// wait for the workers to finish
+					// then close the results channel
+					wg.Wait()
+					close(results)
+				}
+			}
+		}()
+	}
+
+	// send the work to the workers
+	// this happens in a goroutine in order
+	// to not block the main function,
+	// once all workers are busy
+	go func(ctx context.Context) {
+		// send all string combinations to the hash workers
+		fmt.Println("starting combinator")
+		for i := 0; i < f.NumCombs; i++ {
+			select {
+			case <-ctx.Done(): // if cancel() execute, stop sending more work
+				fmt.Println("combinator got cancel() signal")
+				// close the work channel and exit
+				close(work)
+				return
+				// break
+			default:
+				// get the next combination
+				comb := f.combinator.Next()
+				// stop sending work if we have exceeded the max size
+				if len(comb) > f.MaxSize {
+					fmt.Println("combinator max size exceeded")
+					// cancel()
+					// close(work)
+					// return
+					break
+				} else {
+					// send the combination to the worker
+					fmt.Printf("sending comb:%v\n", comb)
+					work <- comb
+				}
+			}
+		}
+		fmt.Println("done all combinations")
+		// close the work channel after all the work has been send
+		// close(work)
+		// wait for the workers to finish
+		// then close the results channel
+		// wg.Wait()
+		// close(results)
+	}(ctx)
+
+	// collect the results
+	// the iteration stops if the results
+	// channel is closed and the last value
+	// has been received
+	for result := range results {
+		log.Printf("RESULT:%v\n", result)
+		return result.Result, nil
+	}
+
 	return "", fmt.Errorf("No value found for hash %v", f.Wanted)
 }
 
--- hash/hash_test.go
+++ hash/hash_test.go
@@ -20,4 +20,35 @@ func TestHash(t *testing.T) {
 			t.Errorf("got vs want mismatch (-want +got):\n%s", diff)
 		}
 	})
+
+
+	t.Run("test_find_parallel1", func(t *testing.T) {
+		numCombs := 10000
+		maxSize := 2
+		minSize := 0
+		charSet := "abcd"
+		wantedHash := "4a8a08f09d37b73795649038408b5f33"
+		print := true
+		finder := NewHashFinder(numCombs, maxSize, minSize, charSet, wantedHash, print)
+		got, _ := finder.FindParallel()
+		want := "c"
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("got vs want mismatch (-want +got):\n%s", diff)
+		}
+	})
+
+	// t.Run("test_find_parallel_not_found", func(t *testing.T) {
+	// 	numCombs := 10000000 * 10000000 // a big number
+	// 	maxSize := 2
+	// 	minSize := 0
+	// 	charSet := "def"
+	// 	wantedHash := "4a8a08f09d37b73795649038408b5f33"
+	// 	print := true
+	// 	finder := NewHashFinder(numCombs, maxSize, minSize, charSet, wantedHash, print)
+	// 	got, _ := finder.FindParallel()
+	// 	want := "c"
+	// 	if diff := cmp.Diff(want, got); diff != "" {
+	// 		t.Errorf("got vs want mismatch (-want +got):\n%s", diff)
+	// 	}
+	// })
 }
